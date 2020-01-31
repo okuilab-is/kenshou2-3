b9f4a1b549ecdb3ef5d908c043142cf86eb560a2
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:39:27 +0900

    gccss

---
+++ kadai3-ep17063.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+  int i,n;
+  double k,val1,val2,min;
+
+  scanf("%d %lf %lf",&n,&k);
+  min=k+1;
+  val2=k+1;
+  for(i=1;i<n;i++){
+    scanf("%lf",&val1);
+    if(k>=val1){
+      if(min>k){//初めだけ行われる
+        min=val1;  
+      }
+      if(min<val1){
+        min=val1;
+      }
+    }
+  }
+  
+  printf("%lf\n",min);
+
+  return 0;
+}
