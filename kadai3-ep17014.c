f3e2ed6ebe2e022ed01e1d1d791ef88de56bb7f1
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:32:51 +0900

    gccss

--- kadai3-ep17014.c
+++ kadai3-ep17014.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 int main(void){
   int n,i,l=0;
-  float ans,num[128],numb[128];
+  float ans,num[128],nummin[128];
   scanf("%d %f",&n,&ans);
   for(i=0;i<n;i++){
     scanf("%f",&num[i]);
     if(num[i]<=k){
-      numb[l]=num[i];
+      nummin[l]=num[i];
       l++;
     }
   }
   for(i=0;i<l;i++){
-    if(ans>k){
+    if(ans>nummin[i]){
+      ans=nummin[i];
     }
   }
   
