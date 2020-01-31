493ac50defd5e3b614ef436b5abe1383b70b6be8
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:43:22 +0900

    gccss

--- kadai3-ep17112.c
+++ kadai3-ep17112.c
@@ -5,10 +5,11 @@ int main(){
 	double k,x,ans;
 	scanf("%d%lf",&n,&k);
 	x=k+1;
-	for(i=0;x>k;i++){
+	for(i=0;x>k && i<n;i++){
 		scanf("%lf",&x);
 		ans=x;
 	}
+	if(i<n) printf("Under k is None.\n");
 	for(;i<n;i++){
 		scanf("%lf",&x);
 		if(x>ans && x<=k){
