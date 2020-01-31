86dc7f9de2aa4d11aec4c2f51da37e9e9a01f554
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:28:30 +0900

    gccss

--- kadai3-ep17008.c
+++ kadai3-ep17008.c
@@ -4,13 +4,13 @@ int main(){
 	int n,i;
 	double k,l,ANS=-10000.0;
 	scanf("%d",&n);
-	scanf("%e",&k);
+	scanf("%f",&k);
 	for(i=0;i<n;i++){
-		scanf("%e",&l);
+		scanf("%f",&l);
 		if(l<=k){
 			if(ANS<l){ANS=l;}
 		}
 	}
-	printf("%e\n",ANS)
+	printf("%e\n",ANS);
 	return 0;
 }
