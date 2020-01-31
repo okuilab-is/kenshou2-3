9f9b4e44acd56e0f3f65522c3494c183fadf45e7
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:56:41 +0900

    gccss

--- kadai3-ep17112.c
+++ kadai3-ep17112.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
 int main(){
-	int n,i;
-	double k,x,ans;//基準値k、入力用データx、解答ans
-	scanf("%d%lf",&n,&k);
+	int n, i;
+	double k, x, ans;//基準値k、入力用データx、解答ans
+	scanf("%d%lf", &n, &k);
 	x=k+0.1;
-	for(i=0;x>k && i<n;i++){
-		scanf("%lf",&x);
+	for(i=0; x>k && i<n; i++){
+		scanf("%lf", &x);
 		ans=x;}
 	if(i==n){
 		printf("Below reference value is None.\n");
 		return 0;}
-	for(;i<n;i++){
-		scanf("%lf",&x);
+	for(; i<n; i++){
+		scanf("%lf", &x);
 		if(x>ans && x<=k){
 		  ans=x;}}
-	printf("%f\n",ans);
+	printf("%f\n", ans);
 	return 0;}
