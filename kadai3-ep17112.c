dccfaeed96c7b72c8e5d4db24b9b1a073fa7b869
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:57:40 +0900

    gccss

--- kadai3-ep17112.c
+++ kadai3-ep17112.c
@@ -4,11 +4,11 @@ int main(){
 	int n, i;
 	double k, x, ans;//基準値k、入力用データx、解答ans
 	scanf("%d%lf", &n, &k);
-	x=k+0.1;
-	for(i=0; x>k && i<n; i++){
+	x = k+0.1;
+	for(i = 0; x>k && i<n; i++){
 		scanf("%lf", &x);
 		ans=x;}
-	if(i==n){
+	if(i == n){
 		printf("Below reference value is None.\n");
 		return 0;}
 	for(; i<n; i++){
