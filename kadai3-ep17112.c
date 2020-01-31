9c0692f2ec0812c9865993e775c564de706c20f3
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:30:37 +0900

    gccss

--- kadai3-ep17112.c
+++ kadai3-ep17112.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int main(){
-  int n,i;
+	int n,i;
 	double k,x,ans;
-  scanf("%d",&n);
+	scanf("%d",&n);
 	scanf("%lf",&k);
-  for(i=0;i<n;i++){
-	  scanf("%lf",&x);
+	for(i=0;i<n;i++){
+		scanf("%lf",&x);
 	}
-  printf("%f\n",ans);
+	printf("%f\n",ans);
 }
