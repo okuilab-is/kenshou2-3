b7220ec00d0240975b32287149e78a4fe4c5a901
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:53:11 +0900

    gccss

--- kadai3-ep17112.c
+++ kadai3-ep17112.c
@@ -7,18 +7,14 @@ int main(){
 	x=k+1;
 	for(i=0;x>k && i<n;i++){
 		scanf("%lf",&x);
-		ans=x;
-	}
+		ans=x;}
 	if(i>=n){
 		printf("Below reference value is None.\n");
-		return 0;
-	}
+		return 0;}
 	for(;i<n;i++){
 		scanf("%lf",&x);
 		if(x>ans && x<=k){
-		  ans=x;
-		}
-	}
+		  ans=x;}}
 	printf("%f\n",ans);
 	return 0;
 }
