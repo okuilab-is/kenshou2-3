d5fd44b23b429c8824862d95f7b4b749ab5087d3
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:40:12 +0900

    gccss

--- kadai3-EP17126.c
+++ kadai3-EP17126.c
@@ -2,13 +2,16 @@
 
 int main(void){
 	int n,i;
-	scanf("%d",&n);
 	double k,x;
-	scanf("%lf",&k);
-	double a=-100;
+	scanf("%d %lf",&n,&k);
+	double a=k+1;
+	for(i=0;a<=k;i++){
+		scanf("%lf",&a);
+	}
+	n-=i;
 	for(i=0;i<n;i++){
 		scanf("%lf",&x);
-		if(x<=k && a<x){a=x;}
+		if(x<=k;a<k){a=x;}
 	}
 	printf("%lf\n",a);
 }
