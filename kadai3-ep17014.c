f8efa1c77e1220afa3b7ad02b28c9fde519e9586
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:25:50 +0900

    gccss

--- kadai3-ep17014.c
+++ kadai3-ep17014.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 int main(void){
-  int n,i;
-  float k,num[128];
+  int n,i,j=0;
+  float k,num[128],numb[128];
   scanf("%d %f",&n,&k);
   for(i=0;i<n;i++){
-    scanf("%lf",num[i]);
+    scanf("%f",&num[i]);
+    if(num[i]<=k){
+      numb[j]=num[i];
+      j++
+    }
   }
+  
 }
