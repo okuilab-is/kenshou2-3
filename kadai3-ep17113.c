3b620d0718a7a57551bbdebb2337e827590ce0b7
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:24:15 +0900

    gccss

---
+++ kadai3-ep17113.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+  int n,i;
+  double k,a,max;
+  bool F=0;
+  scanf("%d %lf",&n,&k);
+  for(i=0;i<n;i++){
+    scanf("%lf",a);
+    if(k>=a){
+      if(F=0){
+        max=a;
+        F=1;
+      }  
+      if(F=1 && max>=a){
+        max=a;
+      }  
+    }  
+  }
+  prinf("%lf\n",a)
+
+
+}
