f17114e4a02e153aeb1b69838ed656b7d2a7e2f9
ep <ep16018@sti.chubu.ac.jp>
2020-01-31 17:28:41 +0900

    gccss

--- kadai3-ep17046.c
+++ kadai3-ep17046.c
@@ -4,11 +4,12 @@ int main(void){
  int n,i,f=0;
  float k,l,min=0;
  scanf("%d%f",&n,&k);
- for(i=0;i<n;i++;){
+ for(i=0;i<n;i++){
   scanf("%f",&l);
   if(l=<k){
    if(f=0){
     min=l;
+    f=1;
    }else if(l>min){
     min=l;
    }
